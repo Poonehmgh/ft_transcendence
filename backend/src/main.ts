a823a78dac1fa63ff09bb019742ec4b507bb23f8
pooneh <p.moghadda30@gmail.com>
2023-08-18 20:09:49 +0200

    added prisma dependency and db schema

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import {PrismaClient} from "@prisma/client"
+//
+// const prisma = new PrismaClient();
+// to: check prisma connection
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
