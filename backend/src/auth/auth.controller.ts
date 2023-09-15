4225f9ebb2838631398bf8769dfea928ace1d99c
pooneh <p.moghadda30@gmail.com>
2023-09-15 15:24:09 +0200

    42oauth almost there. remaining: jwt guard and strategy

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,34 +2,37 @@ import {Controller, Post, Get, Body, Req, Res, UseGuards} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {AuthDto} from "./dto/auth.dto";
 import {ftAuthGuard} from "./guards/intra_42.guard";
+import {Response} from "express";
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post("signup")
-  signup(@Body() dto: AuthDto){
-    return this.authService.signup(dto);
-  }
+  // @Post("signup")
+  // signup(@Body() dto: AuthDto){
+  //   return this.authService.signup(dto);
+  // }
 
-  @Post("signin")
-  signin(@Body() dto: AuthDto, @Req() req, @Res() res){
-    return this.authService.signin(dto, req, res);
-  }
-  @Get("signout")
-  signout(){
-    return this.authService.signout();
-  }
+  // @Post("signin")
+  // signin(@Body() dto: AuthDto, @Req() req, @Res() res){
+  //   return this.authService.signin(dto, req, res);
+  // }
+  // @Get("signout")
+  // signout(){
+  //   return this.authService.signout();
+  // }
   @Get("/42/login")
   @UseGuards(ftAuthGuard)
   ft_oauth(){
-    return this.authService.ft_oauth();
+    // return this.authService.ft_oauth();
+    return {msg: "ewjvfw;"}
   }
 
   @Get("/42/redirect")
   @UseGuards(ftAuthGuard)
-  ft_redirect(@Req() req, @Res() res: Response){
-    return this.authService.ft_signin(req.user);
-    return {msg:"it is redirected here"}
-  }
-
+  async ft_redirect(@Req() req, @Res() res: Response){
+    const token = await this.authService.ft_signin(req.user);
+    // return {msg:"it is redirected here"}
+    res.cookie("token", token)
+    return res.send("the login was successful!")  }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import {BadRequestException, ForbiddenException, Injectable} from '@nestjs/common';
+import {BadRequestException, ForbiddenException, Injectable, InternalServerErrorException} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
 import {AuthDto} from "./dto/auth.dto";
 import * as bcrypt from "bcrypt";
@@ -6,32 +6,10 @@ import {JwtService} from "@nestjs/jwt";
 import {jwtSecret} from "../utils/constants"
 import {Request, Response} from "express"
 
-
-
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService, private jwt: JwtService) {}
-    async signup(dto: AuthDto){
-        const {email, password, name, badge, intraID, status, avatar} = dto;
-        const foundUser = await this.prisma.user.findUnique({
-            where: {
-                email: email,
-            },
-        });
-        if (foundUser)
-            throw new BadRequestException("Email already exists")
-        const hashedPassword = await this.hashPassword(password);
-        await this.prisma.user.create({
-            data: {         //investigate later: why cannot skip the items that have a default value like avatar?
-                email: email,
-                passHash: hashedPassword,
-                intraID: intraID,
-                name: name,
-            }
-        })
 
-        return ({message: "sign_up was successful"})
-    }
     async ft_signin(user)
     {
         if (!user)
@@ -39,51 +17,14 @@ export class AuthService {
         const foundUser = await this.findUserByEmail(user.email);
         if (!foundUser)
             return this.registerUser(user);
-    }
-
-    async signin(dto: AuthDto, req: Request, res: Response){
-        const {email, password, name, badge, intraID, status, avatar} = dto;
-        const foundUser = await this.prisma.user.findUnique({
-            where: {
-                email: email,
-            },
-        });
-        if (!foundUser)
-            throw new BadRequestException("Wrong credentials.")
-        const isMatched = await this.comparePasswords({password,
-        hash: foundUser.passHash,
-        })
-
-        if (!isMatched)
-            throw new BadRequestException("Password is wrong.")
-        // sign jwt tokens:
-        const token = await this.signToken({
-            id: foundUser.id,
+        console.log("user is found: ")
+        // if already registered, sign in:
+        return this.generateJwtToken({
             email: foundUser.email,
-        });
-        if (!token){
-            throw new ForbiddenException();
-        }
-        res.cookie("tocken", token)
-        // return {token}
-        return res.send("the login was successful!")
-    }
-    async signout(){
-        return ""
-    }
-
-    async hashPassword(password: string){
-        const saltOrRounds = 10;
-        return await bcrypt.hash(password, saltOrRounds);
-    }
-
-    async comparePasswords(input: {password: string, hash:string}){
-        return await bcrypt.compare(input.password, input.hash)
-    }
-
-    async signToken(input:{id: number, email: string}){
-        const payload = input;
-        return this.jwt.signAsync(payload, {secret: jwtSecret});
+            name: foundUser.name,
+            passHash: foundUser.passHash,
+            intraID: foundUser.intraID,
+        })
     }
 
     async ft_oauth(){
@@ -101,15 +42,107 @@ export class AuthService {
 
     async registerUser(user){
         const {id, email, name} = user;
-        const newUser = await this.prisma.user.create({
-            data: {
-                email: email,
-                name: name,
-                passHash: "intra42", //change later
-                intraID: "test",
-            }
-        })
-        console.log("new user is ", newUser);
-        return newUser;
+        try{
+            const newUser = await this.prisma.user.create({
+                data: {
+                    email: email,
+                    name: name,
+                    passHash: "intra42", //change later
+                    intraID: "intra42something",
+                }
+            })
+            console.log("new user is ", newUser);
+            return this.generateJwtToken({
+                email: newUser.email,
+                name: newUser.name,
+                passHash: newUser.passHash,
+                intraID: newUser.intraID,
+            })
+        }
+        catch {
+            console.log("bfkfldfjow")
+            throw new InternalServerErrorException();
+        }
     }
+
+    async generateJwtToken(payload){
+    // generateJwtToken(payload){
+        const token = this.jwt.signAsync(payload, {secret: jwtSecret});
+
+        // const token = this.jwt.sign(payload);
+        if (!token){
+            throw new ForbiddenException();
+        }
+        return token;
+    }
+
+
+
+
+    // async signup(dto: AuthDto){
+    //     const {email, password, name, badge, intraID, status, avatar} = dto;
+    //     const foundUser = await this.prisma.user.findUnique({
+    //         where: {
+    //             email: email,
+    //         },
+    //     });
+    //     if (foundUser)
+    //         throw new BadRequestException("Email already exists")
+    //     const hashedPassword = await this.hashPassword(password);
+    //     await this.prisma.user.create({
+    //         data: {         //investigate later: why cannot skip the items that have a default value like avatar?
+    //             email: email,
+    //             passHash: hashedPassword,
+    //             intraID: intraID,
+    //             name: name,
+    //         }
+    //     })
+    //
+    //     return ({message: "sign_up was successful"})
+    // }
+    // async signin(dto: AuthDto, req: Request, res: Response){
+    //     const {email, password, name, badge, intraID, status, avatar} = dto;
+    //     const foundUser = await this.prisma.user.findUnique({
+    //         where: {
+    //             email: email,
+    //         },
+    //     });
+    //     if (!foundUser)
+    //         throw new BadRequestException("Wrong credentials.")
+    //     const isMatched = await this.comparePasswords({password,
+    //     hash: foundUser.passHash,
+    //     })
+    //
+    //     if (!isMatched)
+    //         throw new BadRequestException("Password is wrong.")
+    //     // sign jwt tokens:
+    //     const token = await this.signToken({
+    //         id: foundUser.id,
+    //         email: foundUser.email,
+    //     });
+    //     if (!token){
+    //         throw new ForbiddenException();
+    //     }
+    //     res.cookie("token", token)
+    //     // return {token}
+    //     return res.send("the login was successful!")
+    // }
+    // async signout(){
+    //     return ""
+    // }
+    //
+    // async hashPassword(password: string){
+    //     const saltOrRounds = 10;
+    //     return await bcrypt.hash(password, saltOrRounds);
+    // }
+    //
+    // async comparePasswords(input: {password: string, hash:string}){
+    //     return await bcrypt.compare(input.password, input.hash)
+    // }
+    //
+    // async signToken(input:{id: number, email: string}){
+    //     const payload = input;
+    //     return this.jwt.signAsync(payload, {secret: jwtSecret});
+    // }
+
 }
