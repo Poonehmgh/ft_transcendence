140b176fa0d00455d58ab87fad2ce27b02add854
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-09-19 15:06:04 +0200

    about to merge

--- backend/src/user/user-dto.ts
+++ backend/src/user/user-dto.ts
@@ -1,42 +1,42 @@
 export class NewUserDTO
 {
-	constructor(
-		public id: number,
-		public passHash: string,
-		public name: string,
-		public email: string,
-	) {}
+    constructor(
+        public id: number,
+        public passHash: string,
+        public name: string,
+        public email: string,
+    ) {}
 }
 
 export class IdAndNameDTO
 {
-	constructor(
-		public id: number,
-		public name: string,
-	) {}
+    constructor(
+        public id: number,
+        public name: string,
+    ) {}
 }
 
 export class ScoreCardDTO
 {
-	constructor(
-		public id: number,
-		public name: string,
-		public badge: string,
-		public matches: number,
-		public wins: number,
-		public winrate: number,
-		public mmr: number,
-		public online: boolean
-	) {}
+    constructor(
+        public id: number,
+        public name: string,
+        public badge: string,
+        public matches: number,
+        public wins: number,
+        public winrate: number,
+        public mmr: number,
+        public online: boolean
+    ) {}
 }
 
 export class FriendListDTO
 {
-	constructor(
-		public friends: number[],
-		public friendReq_out: number[],
-		public friendReq_in: number[],
-		public blocked: number[],
-	) {}
+    constructor(
+        public friends: number[],
+        public friendReq_out: number[],
+        public friendReq_in: number[],
+        public blocked: number[],
+    ) {}
 }
 
