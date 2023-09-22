14632ae1a7f9341ddd3cac1d8af7da1c2ce853d2
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-09-22 10:38:55 +0200

    updating LeaderBoard

--- frontend/src/components/LeaderBoard.tsx
+++ frontend/src/components/LeaderBoard.tsx
@@ -10,35 +10,32 @@ interface rowEntryProp {
     rank: number;
 }
 
-const fetchData = async (n: number): Promise<ScoreCardDTO[]> => {
-    const response: Response = await fetch(`http://localhost:5500/user/leaderboard?top=${n}`, {
-        method: "Get",
-        headers: {
-            //Authorization: `Bearer ${jscookies.get}`,
-            // accepted-content?
-        },
-    });
-    return await response.json();
-}
-
-function LeaderBoard(props: leaderBoardProp): Element {
+function LeaderBoard(props: leaderBoardProp): React.JSX.Element {
     const [leaderTable, setLeaderTable] = useState <ScoreCardDTO[]>([]);
+    
     useEffect(() => {
-        const fetchLeaderBoard = async () => {
-            const data = await fetchData(props.n)
-            setLeaderTable(data);
-        }
-        void fetchLeaderBoard();
+        void fetchAndSet(props.n, setLeaderTable);
     }, [props.n]);
     
     return (
         <ol>
             {leaderTable.map((element: ScoreCardDTO, index: number) => (
-                    <RowEntry scoreCard = {element} rank = {index + 1}/>
-                ))}
+                <RowEntry scoreCard = {element} rank = {index + 1}/>
+            ))}
         </ol>
     );
 }
+
+const fetchAndSet = async (n: number, setter: React.Dispatch<React.SetStateAction<ScoreCardDTO[]>>): Promise<void> => {
+    try {
+        const response = await fetch(`http://localhost:5500/user/leaderboard?top=${n}`);
+        const data = await response.json();
+        setter(data);
+    } catch (error) {
+        console.error('Error fetching leaderboard data:', error);
+        setter([]);
+    }
+};
 
 function RowEntry(props: rowEntryProp): React.JSX.Element {
     return (
