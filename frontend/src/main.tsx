b776737a6dbf2d4f0cb65868029606092a70a0b1
pooneh <p.moghadda30@gmail.com>
2023-09-22 18:18:45 +0200

    the front working

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./components/Router.tsx";
 import './styles/style.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
