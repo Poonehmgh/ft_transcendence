174d9994e605c7929588cdc336807b8850aa1ab3
Santiago Tena Hernandez <stenahe@gmail.com>
2023-09-25 01:23:47 +0200

    Abstract getUserID

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -7,42 +7,7 @@ import Chat from "./Chat";
 import ErrorPage from "./ErrorPage";
 import React from "react";
 import Profile from "./Profile/Profile";
-
-const success = true; // Set this to true for a successful operation, or false for an error.
-// const success = false; // Set this to true for a successful operation, or false for an error.
-
-function getUserID(): Promise<string> {
-  return new Promise((resolve, reject) => {
-    // Simulate an asynchronous operation, e.g., fetching data from an API.
-    setTimeout(() => {
-      if (success) {
-        // If the operation was successful, resolve the Promise with a result.
-        resolve('UserID');
-      } else {
-        // If there was an error, reject the Promise with an error message.
-        reject(null);
-      }
-    }, 100); // Simulated delay of 2 seconds
-  });
-}
-
-async function routeUser(){
-  try {
-    await getUserID();
-    return null;
-  } catch (error) {
-    return redirect("/");
-  }
-}
-
-async function routeUserAtLogIn(){
-  try {
-    await getUserID();
-    return redirect("/home");
-  } catch (error) {
-    return null;
-  }
-}
+import {getUserID, routeUser, routeUserAtLogIn} from "../functions/getUserID";
 
 const Router = () => {
     const router = createBrowserRouter([
