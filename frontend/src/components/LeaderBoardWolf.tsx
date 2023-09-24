9a97b95ee7df222d3530cd5ea77d6485d8ab1bcb
Santiago Tena Hernandez <stenahe@gmail.com>
2023-09-24 13:52:02 +0200

    Fix leaderboard

--- frontend/src/components/LeaderBoardWolf.tsx
+++ frontend/src/components/LeaderBoardWolf.tsx
@@ -11,18 +11,17 @@ interface rowEntryProp {
 }
 
 function LeaderBoardWolf(props: leaderBoardProp): React.JSX.Element {
-    const [leaderTable, setLeaderTable] = useState <ScoreCardDTO[]>([]);
-    
+    const [leaderTable, setLeaderTable] = useState <ScoreCardDTO[]>([])
     useEffect(() => {
         void fetchAndSet(props.n, setLeaderTable);
     }, [props.n]);
     
     return (
-        <ol>
-            {leaderTable.map((element: ScoreCardDTO, index: number) => (
-                <RowEntry scoreCard = {element} rank = {index + 1}/>
-            ))}
-        </ol>
+        <tbody>
+        {leaderTable.map((element: ScoreCardDTO, index: number) => (
+            <RowEntry scoreCard = {element} rank = {index + 1}/>
+        ))}
+        </tbody>
     );
 }
 
@@ -39,13 +38,12 @@ const fetchAndSet = async (n: number, setter: React.Dispatch<React.SetStateActio
 
 function RowEntry(props: rowEntryProp): React.JSX.Element {
     return (
-        <li key = {props.rank}>
-            <span>{props.scoreCard.name} </span>
-            <span style={{ fontStyle: 'italic' }}>the {props.scoreCard.title}</span>
-            <span> | mmr: {props.scoreCard.mmr} </span>
-            <span> | matches: {props.scoreCard.matches} </span>
-            <span> | winrate: {props.scoreCard.winrate !== null ? props.scoreCard.winrate : "N/A"}</span>
-        </li>
+        <tr key = {props.rank}>
+            <td>{props.scoreCard.name} the {props.scoreCard.title} </td>
+            <td>{props.scoreCard.mmr} </td>
+            <td>{props.scoreCard.matches} </td>
+            <td>{props.scoreCard.winrate !== null ? props.scoreCard.winrate : "N/A"}</td>
+        </tr>
     )
 }
 
--- frontend/src/components/Leaderboard.tsx
+++ frontend/src/components/Leaderboard.tsx
@@ -1,36 +1,45 @@
 import {Link} from "react-router-dom";
 import LeaderBoardWolf from "./LeaderBoardWolf";
 import React from "react";
+import '../styles/leaderboard.css';
 
 function Leaderboard() {
-    return (
-        <div className="sections-container">
-            <div className="section" id="header">
-                <div className={"pongers-logo"}>
-                    <Link className={"link"} to="/home">Pongers</Link><br/>
-                </div>
-                <ul className={"upper-links"}>
-                    <li>
-                        <Link className={"link"} to="/leaderboard">Leaderboard</Link>
-                    </li>
-                    <li>
-                        <Link className={"link"} to="/game">Game</Link>
-                    </li>
-                    <li>
-                        <Link className={"link"} to="/chat">Chat</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="section" id="right-bar">Right Bar</div>
-            <div className="section" id="center">
-                <div>Leaderboard
-                     <LeaderBoardWolf n = {5} />
-                </div>
-            </div>
-            <div className="section" id="left-bar">Left Bar</div>
-            <div className="section" id="footer">Footer</div>
+  return (
+      <div className="sections-container">
+        <div className="section" id="header">
+          <div className={"pongers-logo"}>
+            <Link className={"link"} to="/home">Pongers</Link><br/>
+          </div>
+          <ul className={"upper-links"}>
+            <li>
+              <Link className={"link"} to="/leaderboard">Leaderboard</Link>
+            </li>
+            <li>
+              <Link className={"link"} to="/game">Game</Link>
+            </li>
+            <li>
+              <Link className={"link"} to="/chat">Chat</Link>
+            </li>
+          </ul>
         </div>
-    );
+        <div className="section" id="center">
+          <div>
+            <h2>Leaderboard</h2>
+            <table>
+              <thead>
+              <tr>
+                <th>Name</th>
+                <th>MMR</th>
+                <th>Matches</th>
+                <th>Win Rate</th>
+              </tr>
+              </thead>
+              <LeaderBoardWolf n = {5} />
+            </table>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default Leaderboard;
