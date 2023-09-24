0ae7905fd531507345b46a7c88d8fc2a5897a8c4
Santiago Tena Hernandez <stenahe@gmail.com>
2023-09-24 21:24:59 +0200

    Remove unused import

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import Header from "../Header";
 import React from "react";
 import '../../styles/profile.css';
+import UserProfile from "../UserProfile/UserProfile";
 
 function Profile() {
   return (
