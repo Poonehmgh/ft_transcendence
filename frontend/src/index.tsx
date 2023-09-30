8288b72a07bada645deb61faf5960d47b96b88ed
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-09-30 08:52:02 +0200

    "revert changes in index.tsx (still have to figure out how to select "root" element)"

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // what's up with this?
-//import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import Router from "./components/Router";
-import Modal from "react-modal";
 import "./styles/style.css";
 
 
