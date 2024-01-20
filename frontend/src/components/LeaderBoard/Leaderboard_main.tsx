4c52f979b0380e40f5f7b4bfe9f23dd4f13494d6
Wolf Arif Mardin <wolf.mardin@gmail.com>
2024-01-20 21:24:00 +0100

    saveState before messing with userTable

--- frontend/src/components/LeaderBoard/Leaderboard_main.tsx
+++ frontend/src/components/LeaderBoard/Leaderboard_main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Header/Header_main";
 import UserTable from "../shared/UserTable";
 import RankNumberColumn from "./RankNumberColumn";
@@ -6,19 +6,24 @@ import RankNumberColumn from "./RankNumberColumn";
 // CSS
 import "src/styles/userTable.css";
 import "src/styles/style.css"
-
+import { UserProfileDTO } from "user-dto";
+import { fetchGetSet } from "src/ApiCalls/fetchers";
 
 function Leaderboard() {
+    const [userList, setUserlist] = useState<UserProfileDTO[]>([]);
     const getTopN = 3;
     const apiUrl = process.env.REACT_APP_BACKEND_URL + "/user/leaderboard?top=" + getTopN;
 
+    useEffect(() => {
+        fetchGetSet(apiUrl, setUserlist);
+    }, []);
 
     return (
             <div className="table-center">
                 <div>
                     <div className = "h2" >Leaderboard</div>
                     <div className="tablesContainer">
-                        <RankNumberColumn topN={getTopN}/>
+                        <RankNumberColumn topN={userList.length}/>
                         <UserTable apiUrl={apiUrl} />
                     </div>
                 </div>
