c9df463a5e3699ae6b0d2a9de5b6b9a3b5cabc60
Wolf Arif Mardin <wolf.mardin@gmail.com>
2024-01-28 22:22:17 +0100

    update matchhistory

--- frontend/src/components/UserProfileModal/MatchHistory.tsx
+++ frontend/src/components/UserProfileModal/MatchHistory.tsx
@@ -29,26 +29,41 @@ function MatchHistory(props: matchHistoryProps): React.JSX.Element {
     if (matches.length === 0) return <div className="p">No matches played.</div>;
 
     return (
-        <table className="matchHistoryTable">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Player1</th>
-                    <th>Player2</th>
-                    <th>Winner</th>
-                </tr>
-            </thead>
-            <tbody>
-                {matches.map((element: MatchDTO) => (
-                    <tr key={element.id}>
-                        <td>{getCalendarDay(element.begin)}</td>
-                        <td>{`${element.player1_id} (${element.score_p1})`}</td>
-                        <td>{`${element.player2_id} (${element.score_p2})`}</td>
-                        <td>{element.winner_name ? element.winner_name : "- DRAW -"}</td>
+        <div style={{ marginBottom: "20px" }}>
+            <div className="h2Left" style={{ fontSize: "1.2rem" }}>
+                Match History
+            </div>
+            <table className="matchHistoryTable">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>{"Player 1 \n (score)"}</th>
+                        <th>{"Player 2 \n (score)"}</th>
+                        <th>Winner</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {matches.map((element: MatchDTO) => (
+                        <tr key={element.id}>
+                            <td>{getCalendarDay(element.begin)}</td>
+                            <td>
+                                {element.player1_id}
+                                <br />
+                                {`(${element.score_p1})`}
+                            </td>
+                            <td>
+                                {element.player2_id}
+                                <br />
+                                {`(${element.score_p2})`}
+                            </td>
+                            <td>
+                                {element.winner_name ? element.winner_name : "- DRAW -"}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
