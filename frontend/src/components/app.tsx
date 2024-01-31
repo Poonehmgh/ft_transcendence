f91db01f971ef9628ee396f79fe2f41ea6bef82d
Wolf Arif Mardin <wolf.mardin@gmail.com>
2024-01-31 08:33:20 +0100

    update userprofilemodal

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "src/components/Header/Header_main";
 import Leaderboard from "src/components/LeaderBoard/Leaderboard_main";
