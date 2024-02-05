84cd65eea80a739d464f95ac891d68c367568a37
Wolf Arif Mardin <wolf.mardin@gmail.com>
2024-02-05 22:10:23 +0100

    auto group naming

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -4,9 +4,10 @@ import { ChatGateway } from './chat.gateway';
 import {ChatController} from "./chat.controller";
 import {PrismaService} from "../prisma/prisma.service";
 import {ChatGatewayService} from "./chat.gateway.service";
+import { UserService } from 'src/user/user.service';
 
 @Module({
   controllers: [ChatController] ,
-  providers: [ChatService, ChatGateway, PrismaService, ChatGatewayService]
+  providers: [ChatService, ChatGateway, PrismaService, ChatGatewayService, UserService]
 })
 export class ChatModule {}
