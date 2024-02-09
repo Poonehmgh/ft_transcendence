fa748503acab96919b62a958c80358a6391ad569
statvej <statvej@gmail.com>
2024-02-09 21:45:17 +0100

    feat: added synchronization of socket between gateways

--- backend/src/chat/chat.gateway.service.ts
+++ backend/src/chat/chat.gateway.service.ts
@@ -71,7 +71,7 @@ export class ChatGatewayService {
 		}
 	}
 
-	async isUserMuted(chatId: number, userId: number): Promise<Boolean>{
+	async isUserMuted(chatId: number, userId: number): Promise<Boolean> {
 		const chatUser = await this.prisma.chat_User.findFirst({
 			where: {
 				userId: userId,
@@ -80,7 +80,7 @@ export class ChatGatewayService {
 		});
 		if (chatUser.muted && chatUser.muted_until > new Date()) {
 			return true;
-		} else if(chatUser.muted && chatUser.muted_until < new Date()) {
+		} else if (chatUser.muted && chatUser.muted_until < new Date()) {
 			await this.prisma.chat_User.updateMany({
 				where: {
 					userId: userId,
@@ -94,7 +94,7 @@ export class ChatGatewayService {
 		return false;
 	}
 
-	async isUserBanned(chatId: number, userId: number): Promise<Boolean>{
+	async isUserBanned(chatId: number, userId: number): Promise<Boolean> {
 		const chatUser = await this.prisma.chat_User.findFirst({
 			where: {
 				userId: userId,
@@ -144,6 +144,36 @@ export class ChatGatewayService {
 			});
 		} catch (error) {
 			console.log(`error in setUserOnlineStatus: ${error.message}`);
+		}
+	}
+
+	async setUserSockerID(userID: number, socketID: string) {
+		try {
+			await this.prisma.user.updateMany({
+				where: {
+					id: Number(userID),
+				},
+				data: {
+					socketId: String(socketID),
+				},
+			});
+		} catch (error) {
+			console.log(`error in setUserSocketID: ${error.message}`);
+		}
+	}
+
+	async deleteUserSocketID(userID: number) {
+		try {
+			await this.prisma.user.updateMany({
+				where: {
+					id: Number(userID),
+				},
+				data: {
+					socketId: "",
+				},
+			});
+		} catch (error) {
+			console.log(`error in deleteUserSocketID: ${error.message}`);
 		}
 	}
 
