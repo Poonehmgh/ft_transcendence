ce2244fb59082c099380254f868d50d9634eb23a
Santiago Tena Hernandez <stenahe@gmail.com>
2024-02-11 23:33:51 +0100

    feat: setup ballStyle

--- frontend/src/components/Game/Ball.tsx
+++ frontend/src/components/Game/Ball.tsx
@@ -1,25 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, { CSSProperties, useEffect, useState } from "react";
 
 function Ball(newRound, gameUpdate) {
-	const [ballPosition, setBallPosition] = useState(null);
+  const [ballPosition, setBallPosition] = useState(null);
 
-	useEffect(() => {
-		setBallPosition(newRound.PositionBall);
-	}, [newRound]);
+  useEffect(() => {
+    setBallPosition(newRound.PositionBall);
+  }, [newRound]);
 
-	useEffect(() => {
-		setBallPosition(gameUpdate.updatedBallPosition);
-	}, [gameUpdate]);
+  useEffect(() => {
+    setBallPosition(gameUpdate.updatedBallPosition);
+  }, [gameUpdate]);
 
-	return (
-		<>
-			{ballPosition ? (
-				<div>Ball</div>
-			):(
-				<></>
-			)}
-		</>
-	)
+  const ballStyle: CSSProperties = {
+    position: "absolute",
+    left: `${ballPosition[0]}px`,
+    top: `${ballPosition[1]}px`,
+    width: `${newRound.ballRadius * 2}px`,
+    height: `${newRound.ballRadius * 2}px`,
+    borderRadius: "50%",
+    backgroundColor: "white",
+  };
+
+  return (
+    <>
+      {newRound ? (
+        <div className="plank" style={ballStyle}>
+          Ball
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
-export default Ball
+export default Ball;
