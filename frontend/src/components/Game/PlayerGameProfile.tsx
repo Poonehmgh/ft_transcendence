9bddaad930123611bbf0aaa48e4eab4d89ad595e
Santiago Tena Hernandez <stenahe@gmail.com>
2024-02-11 23:34:40 +0100

    fix: format

--- frontend/src/components/Game/PlayerGameProfile.tsx
+++ frontend/src/components/Game/PlayerGameProfile.tsx
@@ -2,38 +2,39 @@ import React from "react";
 
 import "src/styles/playerCardTable.css";
 
-function PlayerGameProfile({user}) {
-    return (
-        <>
-            {user ? (
-                <table className="playerCardTable">
-                    <tbody>
-                        <tr>
-                            <td>name</td>
-                            <td>{user.name ? user.name : "-"}</td>
-                        </tr>
-                        <tr>
-                            <td>mmr</td>
-                            <td>{user.mmr ? user.mmr : "-"}</td>
-                        </tr>
-                        <tr>
-                            <td>rank</td>
-                            <td>{user.rank ? user.rank : "-"}</td>
-                        </tr>
-                        <tr>
-                            <td>matches</td>
-                            <td>{user.matches ? user.matches : "-"}</td>
-                        </tr>
-                        <tr>
-                            <td>win rate</td>
-                            <td>{user.winrate ? user.winrate : "-"}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            ) : ( <></> )
-            }
-        </>
-    );
+function PlayerGameProfile({ user }) {
+  return (
+    <>
+      {user ? (
+        <table className="playerCardTable">
+          <tbody>
+            <tr>
+              <td>name</td>
+              <td>{user.name ? user.name : "-"}</td>
+            </tr>
+            <tr>
+              <td>mmr</td>
+              <td>{user.mmr ? user.mmr : "-"}</td>
+            </tr>
+            <tr>
+              <td>rank</td>
+              <td>{user.rank ? user.rank : "-"}</td>
+            </tr>
+            <tr>
+              <td>matches</td>
+              <td>{user.matches ? user.matches : "-"}</td>
+            </tr>
+            <tr>
+              <td>win rate</td>
+              <td>{user.winrate ? user.winrate : "-"}</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
 export default PlayerGameProfile;
