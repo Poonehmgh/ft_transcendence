9774aac9640b8db52b182348ad25a1b05d7b2b64
Santiago Tena Hernandez <stenahe@gmail.com>
2024-02-11 22:10:52 +0100

    fix: modify null rendering

--- frontend/src/components/Game/ScoreV2.tsx
+++ frontend/src/components/Game/ScoreV2.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const ScoreV2 = (newRound) => {
-	return(
-			<span className="score-container">
-					<div className="scores">{newRound.ScorePlayer1}    :    {newRound.ScorePlayer2}</div>
-			</span>
-	);
-}
+  return (
+    <>
+      {newRound ? (
+        <div className="scores">
+          {newRound.ScorePlayer1} : {newRound.ScorePlayer2}
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
 
 export default ScoreV2;
