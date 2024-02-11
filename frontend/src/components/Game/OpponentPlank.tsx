480f39505577c7384c8ed1de0c9e0f3591b1eed0
Santiago Tena Hernandez <stenahe@gmail.com>
2024-02-11 23:34:29 +0100

    feat: setup plankStyle

--- frontend/src/components/Game/OpponentPlank.tsx
+++ frontend/src/components/Game/OpponentPlank.tsx
@@ -1,32 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { CSSProperties, useEffect, useState } from "react";
 
 function OpponentPlank(props) {
-	const { newRound, gameUpdate, isPlayerOne } = props;
-	const [plankPosition, setPlankPosition] = useState(null)
+  const { newRound, gameUpdate, isPlayerOne } = props;
+  const [plankPosition, setPlankPosition] = useState(null);
 
-	useEffect(() => {
-		if (newRound) {
-			if (isPlayerOne) {
-				setPlankPosition(newRound.PositionPlank2)
-			} else {
-				setPlankPosition(newRound.PositionPlank1)
-			}
-		}
-	}, [isPlayerOne, newRound]);
+  useEffect(() => {
+    if (newRound) {
+      if (isPlayerOne) {
+        setPlankPosition(newRound.PositionPlank2);
+      } else {
+        setPlankPosition(newRound.PositionPlank1);
+      }
+    }
+  }, [isPlayerOne, newRound]);
 
-	useEffect(() => {
-		setPlankPosition(gameUpdate.enemyPlankPosition);
-	}, [gameUpdate]);
+  useEffect(() => {
+    setPlankPosition(gameUpdate.enemyPlankPosition);
+  }, [gameUpdate]);
 
-	return (
-		<>
-			{plankPosition ? (
-				<div>OpponentPlank</div>
-			) : (
-				<></>
-			)}
-		</>
-	)
+  const plankStyle: CSSProperties = {
+    position: "absolute",
+    top: `${plankPosition}px`,
+    width: `${newRound.plankWidth}px`,
+    height: `${newRound.plankHeight}px`,
+    backgroundColor: "white",
+  };
+
+  return (
+    <>
+      {newRound ? (
+        <div className="plank" style={plankStyle}>
+          OpponentPlank
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
 export default OpponentPlank;
